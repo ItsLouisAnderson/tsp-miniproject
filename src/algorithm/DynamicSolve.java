ff7a3968374331a08961d5756a84a6508165794d
Huynh Tuan Dung <Huynh Tuan Dung - 20194744>
2021-06-03 15:56:12 +0700

    Thank you Tri for your implementations of DynamicSolve and NaiveSolve. Still can't make them step-by-step though

--- src/algorithm/DynamicSolve.java
+++ src/algorithm/DynamicSolve.java
@@ -3,44 +3,89 @@ package algorithm;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
 import java.util.Collections;
 import java.util.HashMap;
 
-import model.City;
-import model.Graph;
+import model.*;
 
 public class DynamicSolve extends SolveStrategy{
+	public double [][] path_2d;
 	private String name = "Dynamic Programming";
+	
+	private int n;
+	private int VISITED_ALL;
+
+	private Double [][] visted_path_length;
+	private ArrayList [][] visted_path;
 	
 	@Override
 	public String toString() {
 		return this.name;
 	}
-	
-	public double cost(List<City> l, City i, City start) {
-		if (l.size() == 1) {
-			return i.getDistance(start);
+
+	protected List<Path> tsp(int mask, int pos, Graph graph) {
+		List<Path> p = new ArrayList<Path>();
+
+		if (mask == VISITED_ALL) {
+			p.add(new Path(graph.getCityList().get(pos), graph.getCityList().get(0)));
+					//, path_2d[pos][0]);
+			return p;
 		}
-		else {
-			l.remove(i);
-			Map<City, Double> tmp = new HashMap<City, Double>();
-			for (City j: l) {
-				tmp.put(j, Double.valueOf(cost(l, j, start)));
+
+		if (visted_path_length[mask][pos] != -1) {
+			p.addAll(visted_path[mask][pos]);
+			//p.setLength(visted_path_length[mask][pos]);
+			return p;
+		}
+
+		List<Path> tmp;
+		for (int city = 0; city < n; city++) {
+
+			if ((mask & (1 << city)) == 0) {
+
+				tmp = tsp(mask | (1 << city), city, graph);
+				tmp.add(new Path(graph.getCityList().get(pos), graph.getCityList().get(city)));
+						//, path_2d[pos][city]);
+
+				if(getLength(p) == 0){
+					p.clear();
+					p.addAll(tmp);
+					continue;
+				}
+
+				if (getLength(tmp) < getLength(p)){
+					p.clear();
+					p.addAll(tmp);
+				}
 			}
-			Double min = Collections.min(tmp.values());
-			return min;
+
+		}
+
+		ArrayList<Path> tmp2 = new ArrayList<>();
+		tmp2.addAll(p);
+		visted_path[mask][pos] = tmp2;
+		visted_path_length[mask][pos] = getLength(p);
+		return p;
+	}
+
+	private void create_2d_dynamic(Graph graph){
+		create_2d(graph);
+		n = graph.getCityList().size();
+		VISITED_ALL = (1<<n) -1;
+		visted_path_length = new Double[1<<n][n];
+		visted_path = new ArrayList[1<<n][n];
+
+		for(int i=0; i<(1<<n); i++){
+			for(int j=0; j<n; j++){
+				visted_path_length[i][j] = -1.0;
+			}
 		}
 	}
-	
-	public double solve(Graph g) {
-		List<City> cityList = new ArrayList<City>();
-		cityList.addAll(g.getCityList());
-		if (cityList.size() < 2) {
-			return 0.0;
-		}
-		City start = cityList.get(0);
-		cityList.remove(start);
-		City end = cityList.get(cityList.size()-1);
-		return cost(cityList, end, start);
+
+	public void solve(Graph graph){
+		create_2d_dynamic(graph);
+		result = tsp(1,0, graph);
+		//return this;
 	}
 }
--- src/algorithm/NaiveSolve.java
+++ src/algorithm/NaiveSolve.java
@@ -1,15 +1,131 @@
 package algorithm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import model.*;
 
 public class NaiveSolve extends SolveStrategy{
 	private String name = "Naive Programming";
 	
+	@Override
 	public String toString() {
 		return this.name;
 	}
 	
-	public double solve(Graph graph) {
-		return 0;
-	}
+	private void create_2d_Naive(Graph graph){
+        create_2d(graph);
+    }
+
+    private List<Path> exe(Graph graph){
+        int number_of_city = graph.getCityList().size();
+        List<List<Integer>> permutList = getPermut(number_of_city, false);
+        List<Path> tmp  = new ArrayList<Path>();
+        List<Path> main = new ArrayList<Path>();
+
+        for (List<Integer> permut : permutList) {
+
+            for(int i = 0; i < number_of_city-1; i++){
+                if(i==0){
+                    int i1 = permut.get(0);
+
+                    City y0 = graph.getCityList().get(0);
+                    City y1 = graph.getCityList().get(i1);
+
+                    Path e1 = new Path(y0, y1);
+
+                    tmp.add(e1);
+                    //, path_2d[0][y1.getId()-1]);
+                }
+
+                if(i==number_of_city-2){
+                    int iz = permut.get(i);
+
+                    City yz = graph.getCityList().get(iz);
+                    City y0 = graph.getCityList().get(0);
+
+                    Path e = new Path(yz, y0);
+
+                    tmp.add(e);
+                    //, path_2d[yz.getId()-1][0]);
+                    continue;
+                }
+
+                int m = permut.get(i);
+                int n = permut.get(i+1);
+                City ym = graph.getCityList().get(m);
+                City yn = graph.getCityList().get(n);
+
+                Path e = new Path(ym, yn);
+
+                tmp.add(e);
+                //, path_2d[ym.getId()-1][yn.getId()-1]);
+            }
+
+            if(getLength(main) == 0){
+            	main.clear();
+            	main.addAll(tmp);
+            	tmp.clear();
+                continue;
+            }
+
+            if(getLength(tmp) < getLength(main)){
+                main.clear();
+                main.addAll(tmp);
+                tmp.clear();
+            }
+
+            tmp.clear();
+        }
+
+        return main;
+    }
+    private List<List<Integer>> getPermut(int n, boolean enable_print){
+        int[] arr = new int[n-1];
+        for(int i = 0; i < n-1; i++){
+            arr[i] = i+1;
+        }
+
+        List<List<Integer>> list = new ArrayList<>();
+        permutation(list, new ArrayList<>(),arr);
+
+
+
+        if (enable_print){
+            System.out.println("Array : ");
+            for(int i = 0; i < n-1; i++){
+                System.out.println(arr[i]);
+            }
+
+            System.out.println("Permuations of array : ");
+            System.out.println("=========================================");
+            for(List<Integer> perm : list) {
+                System.out.println(perm);
+            }
+        }
+
+        return list;
+
+    }
+    private void permutation (List<List<Integer>> list, List<Integer> resultList, int [] arr){
+        if(resultList.size() == arr.length){
+            list.add(new ArrayList<>(resultList));
+        }
+        else{
+            for (int j : arr) {
+                if (resultList.contains(j)) {
+                    continue;
+                }
+                resultList.add(j);
+                permutation(list, resultList, arr);
+                resultList.remove(resultList.size() - 1);
+            }
+        }
+    }
+    
+    public void solve(Graph graph){
+        create_2d_Naive(graph);
+        result = exe(graph);
+        //return this;
+    }
 }
